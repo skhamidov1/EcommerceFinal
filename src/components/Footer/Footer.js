282fb64a13fd967bc6f91d19d9fa6d01dd5826c8
Seyfat Khamidov <skhamidov@rv-clt-skhamidov.redventures.net>
2018-09-16 22:39:07 -0400

    added rel attribute to links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,12 +6,12 @@ const footer = () => {
         <footer className="footer">
             <nav className="footer__nav">
                 <ul className="footer__unord-list">
-                    <li className="footer__list-item"><Link to="/home">Home</Link></li>
-                    <li className="footer__list-item"><Link to="/products">Products</Link></li>
+                    <li className="footer__list-item"><Link to="/">Home</Link></li>
+                    <li className="footer__list-item"><Link to="/inventory">Products</Link></li>
                     <li className="footer__list-item"><Link to="/contact">Contact</Link></li>
-                    <li className="footer__list-item"><Link to="/facebook">Facebook</Link></li>
-                    <li className="footer__list-item"><Link to="/instagram">Instagram</Link></li>
-                    <li className="footer__list-item"><Link to="/linkedin">Linkedin</Link></li>
+                    <li className="footer__list-item"><a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/">Facebook</a></li>
+                    <li className="footer__list-item"><a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/">Instagram</a></li>
+                    <li className="footer__list-item"><a rel="noopener noreferrer" target="_blank" href="https://www.twitter.com/">Twitter</a></li>
                 </ul>
             </nav>
         </footer>
